b185d365b50ea0eaefb3b31094144d20dd93c09f
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-03-19 14:43:34 -0600

    ready for surge deployement

--- src/app.js
+++ src/app.js
@@ -44,14 +44,12 @@ render(){
                                 <input type="text" id="fname" placeholder="First Name"></input><br></br><br></br>
                                 <input type="text" id="lname" placeholder="Last Name"></input><br></br><br></br>
                                 <input type="text" id="email" placeholder="Email"></input><br></br><br></br><br></br>
-                                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Subscribe</button>
+                                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal2">Subscribe</button>
                             </form><br></br>
                         </div>
                         <div class="card2">
                         <h3 class="adminSignIn">Sign in as a Pivot Administrator to Create a New Blog Posts</h3><br></br>
                             <form action="/action_page.php">
-                                <input type="text" id="Fname" placeholder="First Name"></input><br></br><br></br>
-                                <input type="text" id="Lname" placeholder="Last Name"></input><br></br><br></br>
                                 <input type="text" id="pword" placeholder="Admin Password..."></input><br></br><br></br><br></br>
                                 <button type="button" class="btn btn-info" onClick={(e) => {e.preventDefault(); var txt = e.target.previousSibling.previousSibling.previousSibling.previousSibling.value; console.log(txt); if (txt!=="PVTadmin1") {this.setState({alert: true, adminPost: false});} else{this.setState({adminPost: true, alert: false})}}}>Sign In To Make a Post</button><br></br><br></br>
                                 {newPost}
@@ -61,6 +59,28 @@ render(){
                     </div>
                 </div>
                 <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Create a Post</h4>
+          </div>
+          <div class="modal-body">
+            <form>
+              <input class="create-input" type="text" placeholder="Post Title"></input><br></br><br></br>
+              <input class="create-input" type="text" placeholder="Author"></input><br></br><br></br>
+              <input class="create-input" type="text" placeholder="Image URL"></input><br></br><br></br>
+              <textarea class="create-body-textarea" placeholder="Post Body"></textarea><br></br><br></br>
+              <button class="create-submit-button" onClick={(e) => {this.handleSubmit(e)}}>Sign Up</button>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div> 
+    <div class="modal fade" id="myModal2" role="dialog">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
